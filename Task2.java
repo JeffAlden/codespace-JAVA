f63478d1e2b979eb0e48d78ce94b2eba7b31b353
JeffAlden <javp14@gmail.com>
2025-05-02 15:07:39 +0800

    Add Task2.java

---
+++ Task2.java
@@ -0,0 +1,15 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        // H3110 w0r1d 2.0 true
+        char H = 'H';
+        byte three = 3;
+        int one = 1;
+        short zero = 0;
+        float f = 2.0f;
+        boolean bool = true;
+
+        String output = "" + H + three + one + one + zero + " w" + zero + "r" + one + "d " + f + " " + bool;
+        System.out.println(output);  
+    }
+}
