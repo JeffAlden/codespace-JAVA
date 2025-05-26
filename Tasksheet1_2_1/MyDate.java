2215fda875330efb9343bea4d057f604f6117fe8
JeffAlden <javp14@gmail.com>
2025-05-26 23:06:21 +0800

    Task sheet 1-2-2

---
+++ Tasksheet1_2_1/MyDate.java
@@ -0,0 +1,52 @@
+package Tasksheet1_2_1;
+
+class MyDateTask {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDateTask() {
+        this.day = 1;
+        this.month = 1;
+        this.year = 2000;
+    }
+
+    public MyDateTask(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public boolean isLeapYear() {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    @Override
+    public String toString() {
+        return day + "/" + month + "/" + year;
+    }
+
+    public void setDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
+
+public class MyDate {
+    public static void main(String[] args) {
+        MyDateTask date1 = new MyDateTask();
+        MyDateTask date2 = new MyDateTask();
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+        // Display leap years
+
+        // Set the date for date1 to January 1st, 1978 and date2 to September 21st, 1984
+        date1.setDate(1, 1, 1978);
+        date2.setDate(21, 9, 1984);
+
+        // Display the dates and check if they are in a leap year
+        System.out.println("Date 1: " + date1.toString() + " - Leap Year: " + date1.isLeapYear());
+        System.out.println("Date 2: " + date2.toString() + " - Leap Year: " + date2.isLeapYear());
+    }
+}
